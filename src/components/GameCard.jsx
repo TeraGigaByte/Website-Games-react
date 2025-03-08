476f631b959d9b67185e7c31bc743ba249a85947
TeraGigaByte <pred_game@mail.ru>
2025-03-08 15:40:13 +0300

    Обновлён CSS

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,11 +1,26 @@
 function GameCard({ game, onFavoriteToggle, isFavorite }) {
+  // Проверяем, есть ли дата релиза. Если нет — выводим 'N/A'.
+  const releaseYear = game.released
+    ? new Date(game.released).getFullYear()
+    : "N/A";
+
   return (
-    <div className="game-card">
-      <img src={game.background_image} alt={game.name} width="200" />
-      <h3>{game.name}</h3>
-      <button onClick={() => onFavoriteToggle(game)}>
-        {isFavorite ? "Удалить из избранного" : "Добавить в избранное"}
-      </button>
+    <div
+      className="game-card"
+      style={{ backgroundImage: `url(${game.background_image})` }}
+    >
+      <div className="game-card-overlay">
+        <h3>{game.name}</h3>
+        <p className="release-year">{releaseYear}</p>
+
+        <button
+          onClick={() => onFavoriteToggle(game)}
+          className="favorite-button"
+          aria-label="Toggle Favorite"
+        >
+          {isFavorite ? "♥" : "♡"}
+        </button>
+      </div>
     </div>
   );
 }
